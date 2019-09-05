30bf5e5bfc27dae2b98bf5aa8426d4850754ec33
Verdang <verdang@media-saturn.com>
2019-09-05 08:20:40 +0200

    Refactoring; new design; buttons change

--- game.js
+++ game.js
@@ -1,33 +1,27 @@
 class Game {
     constructor() {
         this.level = 0;
-        this.soldierImg = story[this.level].soldierImg;
-        this.chapter = story[this.level].chapter;
-        this.storyName = story[this.level].storyName;
-        this.story = story[this.level].story;
+        this.changeLevel();
     }
     nextLevel = () => {
         this.level++;
+        this.changeLevel();
+    }
+    changeLevel = () => {
         this.soldierImg = story[this.level].soldierImg;
         this.chapter = story[this.level].chapter;
         this.storyName = story[this.level].storyName;
         this.story = story[this.level].story;
-        this.storyTelling();
+        this.tot = story[this.level].tot;
+        this.antwortA = story[this.level].antwortA;
+        this.antwortB = story[this.level].antwortB;
+        this.antwortC = story[this.level].antwortC;
     }
     wrongAnswer = () => {
         window.open('loose.html');
         }
     tryAgain =() => {
-        
+        console.log("tryAgain Button")
+        return;
         }
-    storyTelling(){
-        console.log(newGame.level)
-        if (newGame.level < 9){
-        document.getElementById('level').innerHTML = newGame.level;
-        document.getElementById('chapter').innerHTML = ` - ${newGame.chapter}`;
-        document.getElementById('geschichten').innerHTML = newGame.story;
-        document.getElementById('hero-img').src = `IMG/Hero/${newGame.soldierImg}`
-        } else 
-        window.open('winning.html');
-        }  
 }
--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const newGame = new Game();
 
-newGame.storyTelling();
-
-
-
+storyTelling();
 
 // Hier wird der event gestartet was passiert wenn der Button AAAAAAA geklickt wird!
 const antwortA = document.getElementById('btn-AntwortA');
-antwortA.addEventListener('click', newGame.nextLevel);
+//document.getElementById('btn-AntwortA').innerHTML = ` - ${newGame.antwortA}`
+antwortA.addEventListener('click', ()  => {
+    newGame.nextLevel();
+    storyTelling();
+});
 
 // Hier wird der event gestartet was passiert wenn der Button BBBBBBB geklickt wird!
 const antwortB = document.getElementById('btn-AntwortB');
-antwortB.addEventListener('click', newGame.wrongAnswer);
+antwortB.addEventListener('click', ()  => {
+    newGame.wrongAnswer();
+    storyTelling();
+});
 
 // Hier wird der event gestartet was passiert wenn der Button CCCCCCC geklickt wird!
 const antwortC = document.getElementById('btn-AntwortC');
-antwortC.addEventListener('click', newGame.tryAgain);
+antwortC.addEventListener('click', ()  => {
+    newGame.tryAgain();
+    storyTelling();
+});
+
+function storyTelling(){
+    if (newGame.level < 10){
+    document.getElementById('level').innerHTML = newGame.level;
+    document.getElementById('chapter').innerHTML = ` - ${newGame.chapter}`;
+    document.getElementById('geschichten').innerHTML = newGame.story;
+    document.getElementById('hero-img').src = `IMG/Hero/${newGame.soldierImg}`;
+    document.getElementById('btn-AntwortA').innerHTML = newGame.antwortA;
+    document.getElementById('btn-AntwortB').innerHTML = newGame.antwortB;
+    document.getElementById('btn-AntwortC').innerHTML = newGame.antwortC;
+    } else if (newGame.level = 10){
+    document.getElementById('hero-img').src = `IMG/${newGame.soldierImg}`;
+    } else
+    document.getElementById('hero-img').src = `IMG/graveyard.jpg`;
+}
